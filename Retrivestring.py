de3607c914fab16d17686722b2e2c27c58cda9e8
paryanimohit <46473952+paryanimohit@users.noreply.github.com>
2019-03-01 17:50:12 +0530

    Add files via upload

---
+++ Retrivestring.py
@@ -0,0 +1,13 @@
+from pymongo import MongoClient
+import base64
+# connect to MongoDB, change the << MONGODB URL >> to reflect your own connection string
+with open("/home/krypton/Documents/Cam101_03_2019_16_03_33_448477.png", "rb") as imageFile:
+    string = base64.b64encode(imageFile.read())
+try:
+    conn = MongoClient("mongodb+srv://admin:root1234@cluster0-3eho0.mongodb.net/test?retryWrites=true")
+    print("Connected Successfully")
+except:   
+    print("Could not connect to MongoDB")
+db=conn.MajorDatabase
+collection = db.ImageDB
+print(collection.find_one())
