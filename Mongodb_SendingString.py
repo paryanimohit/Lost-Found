90f03fdaf15ecd44271b156543fd5df1b8e98a58
paryanimohit <46473952+paryanimohit@users.noreply.github.com>
2019-03-01 17:07:13 +0530

    Add files via upload

--- Mongodb_SendingString.py
+++ Mongodb_SendingString.py
@@ -1,5 +1,8 @@
 from pymongo import MongoClient
+import base64
 # connect to MongoDB, change the << MONGODB URL >> to reflect your own connection string
+with open("/home/krypton/Documents/Cam101_03_2019_16_03_33_448477.png", "rb") as imageFile:
+    string = base64.b64encode(imageFile.read())
 try:
     conn = MongoClient("mongodb+srv://admin:root1234@cluster0-3eho0.mongodb.net/test?retryWrites=true")
     print("Connected Successfully")
@@ -8,7 +11,7 @@ except:
 db=conn.MajorDatabase
 collection = db.ImageDB
 entry1 = { 
-        "string":"hello"
+        "string":string
         } 
 id1 = collection.insert_one(entry1)
 
