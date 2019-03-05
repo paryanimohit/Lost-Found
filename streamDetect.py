a5483b10ef854f44bfec314138b86c67b2e74e18
paryanimohit <46473952+paryanimohit@users.noreply.github.com>
2019-03-05 16:43:21 +0530

    Update streamDetect.py

--- streamDetect.py
+++ streamDetect.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Mar 05 16:08:53 2019
-
+Created on Sat Feb 23 19:48:53 2019
 @author: Trevor
 """
 
@@ -13,8 +12,8 @@ import os
 # multiple cascades: https://github.com/Itseez/opencv/tree/master/data/haarcascades
 
 #https://github.com/Itseez/opencv/blob/master/data/haarcascades/haarcascade_frontalface_default.xml
-face_cascade = cv2.CascadeClassifier('C:/ProgramData/Anaconda3/Library/share/OpenCV/haarcascades/haarcascade_frontalface_default.xml')
-
+face_cascade = cv2.CascadeClassifier('/home/krypton/Documents/haarcascades/haarcascade_frontalface_default.xml')
+counter=0
 
 cap = cv2.VideoCapture(0) #url for ipcam streaming/video('http://192.168.0.103:8080/video')
 
@@ -24,7 +23,7 @@ while 1:
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
     for (x,y,w,h) in faces:
-       # Image Naming #
+        # Image Naming #
 
         sysTime = datetime.datetime.now()
         deviceTime=sysTime.strftime("%d_%m_%Y_%H_%M_%S_%f")
@@ -35,12 +34,15 @@ while 1:
         cv2.rectangle(img,(x,y),(x+w,y+h),(57,255,20),2)
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = img[y:y+h, x:x+w]
-        
         sub_face = img[y:y+h, x:x+w]
         #cv2.imwrite(imageName,img)
+        path='/home/krypton/Documents/Image'
         cv2.imwrite(os.path.join(path , imageName), sub_face)
-       
-
+        counter=counter+1
+        if(counter==20):
+            os.system("python3 Mongodb_SendingString.py")
+            print("=s***************************")
+            counter=0
     cv2.imshow('img',img)
     k = cv2.waitKey(30) & 0xff
     if k == 27:
