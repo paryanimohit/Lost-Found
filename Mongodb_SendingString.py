0dd4f47c63167fcd9c254ed32bae84ae7f7d6685
paryanimohit <46473952+paryanimohit@users.noreply.github.com>
2019-03-05 16:45:34 +0530

    Update Mongodb_SendingString.py

--- Mongodb_SendingString.py
+++ Mongodb_SendingString.py
@@ -1,18 +1,22 @@
-from pymongo import MongoClient
 import base64
-# connect to MongoDB, change the << MONGODB URL >> to reflect your own connection string
-with open("/home/krypton/Documents/Cam101_03_2019_16_03_33_448477.png", "rb") as imageFile:
-    string = base64.b64encode(imageFile.read())
-try:
-    conn = MongoClient("mongodb+srv://admin:root1234@cluster0-3eho0.mongodb.net/test?retryWrites=true")
-    print("Connected Successfully")
-except:   
-    print("Could not connect to MongoDB")
-db=conn.MajorDatabase
-collection = db.ImageDB
-entry1 = { 
-        "string":string
-        } 
-id1 = collection.insert_one(entry1)
-
-print("Data inserted with record ids",id1)
+import os
+from Connection import MongoConnection
+class UploadImage():
+    def upload(self):
+        j=0
+        collect=MongoConnection.getConnection()
+        listImages=os.listdir("/home/krypton/Documents/Image")
+        
+        for i in listImages:
+            with open("/home/krypton/Documents/Image/"+i, "rb") as imageFile:
+                string = base64.b64encode(imageFile.read())
+                dataDict={"Image"+str(j):
+                            string
+                       }
+                collect.insert_one(dataDict)
+                j=j+1
+                os.remove("/home/krypton/Documents/Image/"+i)
+        print("Data inserted with record ids")
+    
+obj = UploadImage()
+obj.upload()
