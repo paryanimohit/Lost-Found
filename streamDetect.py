50c55e79599a3c1d805efff383d954ab579c8ca1
Mandar Wadnerkar <34623812+mandarw21@users.noreply.github.com>
2019-03-05 15:53:05 +0530

    Crop_image

--- streamDetect.py
+++ streamDetect.py
@@ -14,8 +14,7 @@ import os
 
 #https://github.com/Itseez/opencv/blob/master/data/haarcascades/haarcascade_frontalface_default.xml
 face_cascade = cv2.CascadeClassifier('C:/ProgramData/Anaconda3/Library/share/OpenCV/haarcascades/haarcascade_frontalface_default.xml')
-#https://github.com/Itseez/opencv/blob/master/data/haarcascades/haarcascade_eye.xml
-eye_cascade = cv2.CascadeClassifier('C:/ProgramData/Anaconda3/Library/share/OpenCV/haarcascades/haarcascade_eye.xml')
+
 
 cap = cv2.VideoCapture(0) #url for ipcam streaming/video('http://192.168.0.103:8080/video')
 
@@ -25,23 +24,17 @@ while 1:
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
     for (x,y,w,h) in faces:
-        # Image Naming #
-
-        sysTime = datetime.datetime.now()
-        deviceTime=sysTime.strftime("%d_%m_%Y_%H_%M_%S_%f")
-        imageName = "Cam1"+deviceTime+".png"
-        
         #  Image block complete #
         
-        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+        cv2.rectangle(img,(x,y),(x+w,y+h),(57,255,20),2)
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = img[y:y+h, x:x+w]
-        #cv2.imwrite(imageName,img)
-        cv2.imwrite(os.path.join(path , imageName), img)
         
-        eyes = eye_cascade.detectMultiScale(roi_gray)
-        for (ex,ey,ew,eh) in eyes:
-            cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+        sub_face = img[y:y+h, x:x+w]
+        #cv2.imwrite(imageName,img)
+        cv2.imwrite(os.path.join(path , imageName), sub_face)
+        
+       
 
     cv2.imshow('img',img)
     k = cv2.waitKey(30) & 0xff
