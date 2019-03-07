dd980b28ec6db42b872ba932bedbc86f5fe2f0e4
Mandar Wadnerkar <34623812+mandarw21@users.noreply.github.com>
2019-03-07 15:39:35 +0530

    Img to String and Vice Versa

--- image2String.py
+++ image2String.py
@@ -7,9 +7,22 @@ Created on Fri Mar  1 15:36:38 2019
 
 import base64 
 import sys
- 
+
+string = ""
+
 with open("E:/Programs/Python/images/t.png", "rb") as imageFile:
     string = base64.b64encode(imageFile.read())
     #print(string)
     print(len(string))
     print(sys.getsizeof(string))
+    
+# =============================================================================
+# fh = open("imageToSave.png", "wb")
+# fh.write(string.decode('base64'))
+# fh.close()  
+# =============================================================================
+
+imgdata = base64.b64decode(string)
+filename = 'some_image.png'  # I assume you have a way of picking unique filenames
+with open(filename, 'wb') as f:
+    f.write(imgdata) 
