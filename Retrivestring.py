1a3b271c3ca053d088eec42ffd52266cdbd6fcb0
paryanimohit <46473952+paryanimohit@users.noreply.github.com>
2019-03-09 17:10:17 +0530

    Update Retrivestring.py

--- Retrivestring.py
+++ Retrivestring.py
@@ -1,3 +1,19 @@
 from Connection import MongoConnection
+import base64
+
+a=[]
 collect=MongoConnection.getConnection()
-print(collect.find_one())
+dits=collect.find({},{"Image" : 1})
+i=0
+for dit in dits: 
+    
+    a.append(dit['Image'])    
+    imgdata = base64.b64decode(a[i])
+    filename = str(i)+'.png'  
+    with open('/home/krypton/Documents/RetrivedImages/'+filename, 'wb') as f:
+        f.write(imgdata)
+    i=i+1
+print(str(i)+" Images saved")
+
+collect=MongoConnection.getConnection()
+x=collect.delete_many({})
