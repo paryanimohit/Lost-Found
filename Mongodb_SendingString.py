3d1c7faf8c56e9ef234430c030b745e06ade14b4
PrakharS2811 <42507333+PrakharS2811@users.noreply.github.com>
2019-03-09 17:23:58 +0530

    Updated  Camera_ID":str(j)

--- Mongodb_SendingString.py
+++ Mongodb_SendingString.py
@@ -4,19 +4,18 @@ from Connection import MongoConnection
 class UploadImage():
     def upload(self):
         j=0
+        Images_Path="/home/dark/Programing/Major/Images/"
         collect=MongoConnection.getConnection()
-        listImages=os.listdir("/home/krypton/Documents/Image")
+        listImages=os.listdir(Images_Path)
         
         for i in listImages:
-            with open("/home/krypton/Documents/Image/"+i, "rb") as imageFile:
+            with open(Images_Path+i, "rb") as imageFile:
                 string = base64.b64encode(imageFile.read())
-                dataDict={"Image"+str(j):
-                            string
+                dataDict={"Image":string,
+                          "Camera_ID":str(j)
                        }
                 collect.insert_one(dataDict)
                 j=j+1
-                os.remove("/home/krypton/Documents/Image/"+i)
-        print("Data inserted with record ids")
+                os.remove(Images_Path+i)
+        print(str(j)+" Images inserted in db")
     
-obj = UploadImage()
-obj.upload()
